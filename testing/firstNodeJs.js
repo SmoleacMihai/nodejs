bbacee9327ef621be356808a8f3a702af1fed830
Mihai <mihaismoleac2002@gmail.com>
2023-01-15 22:56:02 +0200

    fle upload using formdable package

--- testing/firstNodeJs.js
+++ testing/firstNodeJs.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var dt = require('./modules/demo_module');
+var dt = require('./modules/currentDate.js');
+var uc = require("upper-case");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
+  res.write(uc.upperCase("The date and time are currently: ") + dt.myDateTime());
   res.end();
 }).listen(8080);
